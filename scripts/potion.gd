7da0e1d7e01857b2a8b396d2b55f2376983d6852
Samer Sawan <samersawan123@gmail.com>
2024-07-29 00:44:47 -0400

    fixed null error in inventory

--- scripts/potion.gd
+++ scripts/potion.gd
@@ -7,24 +7,25 @@ var potion_parent
 func _ready():
 	potion_parent = get_tree().get_first_node_in_group("potion_thrower")
 	linear_velocity = potion_parent.potion_velocity
-	currentPotion = potion_parent.potion_resource.name
-	match currentPotion:
-		"InvisPotion":
-			sprite_2d.frame = 0
-		"SlimePotion":
-			sprite_2d.frame = 1
-		"StatuePotion":
-			sprite_2d.frame = 2
-		"SmokePotion":
-			sprite_2d.frame = 3
-		"StrengthPotion":
-			sprite_2d.frame = 4
-		"NoisePotion":
-			sprite_2d.frame = 5
-		"DashPotion":
-			sprite_2d.frame = 6
-		"SleepPotion":
-			sprite_2d.frame = 7
+	if potion_parent.potion_resource:
+		currentPotion = potion_parent.potion_resource.name
+		match currentPotion:
+			"InvisPotion":
+				sprite_2d.frame = 0
+			"SlimePotion":
+				sprite_2d.frame = 1
+			"StatuePotion":
+				sprite_2d.frame = 2
+			"SmokePotion":
+				sprite_2d.frame = 3
+			"StrengthPotion":
+				sprite_2d.frame = 4
+			"NoisePotion":
+				sprite_2d.frame = 5
+			"DashPotion":
+				sprite_2d.frame = 6
+			"SleepPotion":
+				sprite_2d.frame = 7
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("enemy"):
