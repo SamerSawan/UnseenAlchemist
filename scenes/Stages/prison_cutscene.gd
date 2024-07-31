79fd354d1750d175dde74a1c843534e51f2a2eaf
Totsnuk <evanmaywald2@gmail.com>
2024-07-30 22:50:59 -0500

    skip button, fixed various things, barrel on lvl1, added hotbar back to player, fixed cave ambience not looping

--- scenes/Stages/prison_cutscene.gd
+++ scenes/Stages/prison_cutscene.gd
@@ -1,14 +1,16 @@
 extends CanvasLayer
 
 @export var sprite : Sprite2D
+@export var button : Button
+@export var black_color : ColorRect
 var stage_1_scene : String = "res://scenes/Stages/level_1.tscn"
 
 func _ready():
-	SceneTreeTimer
 	var timer = Timer.new()
 	add_child(timer)
 	timer.start(.5)
 	timer.timeout.connect(_on_timer_timeout)
+	button.pressed.connect(_on_button_pressed)
 
 func _on_timer_timeout():
 	print("timeout")
@@ -16,3 +18,6 @@ func _on_timer_timeout():
 		sprite.frame += 1
 	else:
 		get_tree().change_scene_to_file(stage_1_scene)
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file(stage_1_scene)
