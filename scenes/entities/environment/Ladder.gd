45536c30229e7128c866932e943224c0ce890df1
Samer Sawan <samersawan123@gmail.com>
2024-07-30 22:12:37 -0400

    ladder

---
+++ scenes/entities/environment/Ladder.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+var player
+var ladder = false
+
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+
+func _process(delta):
+	if ladder:
+			player.velocity.y = -200 * Input.get_axis("MoveDown", "MoveUp")
+			player.gravity_value = 0
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		ladder = true
+
+func _on_area_2d_body_exited(body):
+	ladder = false
+	player.gravity_value = 980
